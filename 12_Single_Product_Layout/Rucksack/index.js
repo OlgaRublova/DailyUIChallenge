9487338e8be81e631b9d814210b82b1188a9c37c
olga <olga.i.rubleva@gmail.com>
2019-09-04 14:36:12 -0700

    - added Rucksack product card

---
+++ 12_Single_Product_Layout/Rucksack/index.js
@@ -0,0 +1,9 @@
+$("button.li:nth-child(1)").click(function () {
+    $(".left-set img:nth-child(2)").animate({opacity:1})
+
+});
+
+$("button.li:nth-child(2)").click(function () {
+    $(".left-set img:nth-child(2)").animate({opacity:0})
+
+});
